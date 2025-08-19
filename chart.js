2ff456ac459c12103d1a530135b67a8895b7d23b
rendhiardianto <39326456+rendhiardianto@users.noreply.github.com>
2025-08-19 13:44:35 +0700

    Yes 2

--- chart.js
+++ chart.js
@@ -110,3 +110,135 @@ new Chart("myChart4", {
   }
 });
 }
+
+//============================= Chart 5 =========================
+{
+const xValues = ["Gol I", "Gol II", "Gol III", "Gol IV"];
+const yValues = [55, 49, 44, 24];
+const barColors = "yellow";
+
+new Chart("myChart5", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+
+    title: {
+      display: true,
+      text: "Golongan"
+    }
+  }
+});
+}
+
+//============================= Chart 6 =========================
+{
+const xValues = ["Eselon I", "Eselon II", "Eselon III", "Eselon IV", "Eselon V"];
+const yValues = [55, 49, 44, 24, 50];
+const barColors = "green";
+
+new Chart("myChart6", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+
+    title: {
+      display: true,
+      text: "Eselon"
+    }
+  }
+});
+}
+
+//============================= Chart 7 =========================
+{
+const xValues = ["SD", "SLTP", "SLTA", "DI-DII", "DII", "DIV", "S1", "S2", "S3"];
+const yValues = [55, 49, 44, 24, 24, 30, 100, 30, 10];
+const barColors = "cyan";
+
+new Chart("myChart7", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+
+    title: {
+      display: true,
+      text: "Pendidikan"
+    }
+  }
+});
+}
+
+//============================= Chart 8 =========================
+{
+const xValues = ["Januari 2025", "Februari 2025", "Maret 2025", "April 2025", "Mei 2025", "Juni 2025"];
+const yValues = [55, 49, 44, 24, 25, 27];
+const barColors = "black";
+
+new Chart("myChart8", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+
+    title: {
+      display: true,
+      text: "BUP"
+    }
+  }
+});
+}
