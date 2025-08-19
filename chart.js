d1f6f0a182d6bc9c473474bc92c49c3ea5cc8c5d
rendhiardianto <39326456+rendhiardianto@users.noreply.github.com>
2025-08-19 12:17:10 +0700

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,13 +1,8 @@
-//===================================== Bar ==================================
-const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yValues = [55, 49, 44, 24, 15];
-const barColors = [
-  "#b91d47",
-  "#00aba9",
-  "#2b5797",
-  "#e8c3b9",
-  "#1e7145"
-];
+//===================================== Chart 1 ==================================
+{
+const xValues = ["Laki-Laki", "Perempuan"];
+const yValues = [55, 49];
+const barColors = [ "#b91d47", "#00aba9"];
 
 new Chart("myChart1", {
   type: "doughnut",
@@ -21,17 +16,42 @@ new Chart("myChart1", {
   options: {
     title: {
       display: true,
-      text: "World Wide Wine Production 2018"
+      text: "Jenis Kelamin"
     }
   }
 });
-
-//============================= Donat =========================
-const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yValues = [55, 49, 44, 24, 15];
-const barColors = ["red", "green","blue","orange","brown"];
+}
+// ================================== Chart 2 =======================================
+{
+const xValues = ["PNS", "PPPK"];
+const yValues = [55, 49];
+const barColors = [ "blue", "green"];
 
 new Chart("myChart2", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Status Kepegawaian"
+    }
+  }
+});
+}
+
+//============================= Chart 3 =========================
+{
+const xValues = ["18-20", "21-30", "31-40", "41-50", "51-60", "Diatas 60"];
+const yValues = [55, 49, 44, 24, 15, 17];
+const barColors = "blue";
+
+new Chart("myChart3", {
   type: "bar",
   data: {
     labels: xValues,
@@ -52,7 +72,41 @@ new Chart("myChart2", {
 
     title: {
       display: true,
-      text: "World Wine Production 2018"
+      text: "Usia ASN"
     }
   }
 });
+}
+
+//============================= Chart 4 =========================
+{
+const xValues = ["Baby Boomer", "Generasi X", "Generasi Y", "Generasi Z"];
+const yValues = [55, 49, 44, 24];
+const barColors = "red";
+
+new Chart("myChart4", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+
+    title: {
+      display: true,
+      text: "Generasi"
+    }
+  }
+});
+}
